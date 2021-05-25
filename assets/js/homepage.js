6caafa5a504dd05bd4bb75fd8a31a61a4c271337
Tanner C <tannerc.bootcamp@gmail.com>
2021-05-24 17:12:12 -0700

    User TannerCarter info fetched through api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,14 @@
-var getUserRepos = function(user) {
+var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a get request to url
-  fetch(apiUrl).then(function(response) {
+  fetch(apiUrl).then(function (response) {
     console.log(response);
-    response.json().then(function(data) {
+    response.json().then(function (data) {
       console.log(data);
     });
   });
 };
 
-getUserRepos("lernantino");
+getUserRepos("TannerCarter");
